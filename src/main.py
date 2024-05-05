c06c72a818d729381873864ba04e4ae34b3d832d
prakhar479 <singhalprakhar7@gmail.com>
2024-05-05 07:26:01 +0530

    draw rect

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ PEN_COLOR = 'black'
 def main():
     window = win.Window()
     drawing_line = False
+    drawing_rect = False
     while True:
         event, values = window.event()
         print(event, values)
@@ -17,8 +18,10 @@ def main():
             start_pt = None
             end_pt = None
             
-        # if event == "-RECT-":
-        #     print("Rect")
+        if event == "-RECT-":
+            drawing_rect = True
+            start_pt = None
+            end_pt = None
         
         if drawing_line and event == "-CANVAS-":
             if start_pt is None:
@@ -27,6 +30,14 @@ def main():
                 end_pt = (values["-CANVAS-"][0], values["-CANVAS-"][1])
                 window.canvas.draw_line(start_pt, end_pt, color=PEN_COLOR, width=PEN_SIZE)
                 drawing_line = False
+        
+        if drawing_rect and event == "-CANVAS-":
+            if start_pt is None:
+                start_pt = (values["-CANVAS-"][0], values["-CANVAS-"][1])
+            else:
+                end_pt = (values["-CANVAS-"][0], values["-CANVAS-"][1])
+                window.canvas.draw_rectangle(start_pt, end_pt, fill_color=PEN_COLOR, line_color=PEN_COLOR)
+                drawing_rect = False
                 
     
 if __name__ == "__main__":
