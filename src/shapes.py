5ffbdff4f4877b36ecd8e1952e18686139139d59
Prakhar-Jain-1 <prakharjain582@gmail.com>
2024-05-06 02:48:17 +0530

    Pika CLeaner

--- src/shapes.py
+++ src/shapes.py
@@ -10,21 +10,22 @@ CORNER_TYPE = ["pointed", "CURVY"]
 
 
 class Shape(Object):
-    # def __init__(self, height: int, width: int, centoid: tuple[int, int]) -> None:
-    #     super().__init__(height,width,centoid)
-    #     self.color = DEFAULT_COLOR
+    """
+     This class is a parent class it sets the main template of all the classes making shapes
+    """
 
     def __init__(self, start_point, end_point):
         super().__init__(start_point, end_point)
         self.colour = DEFAULT_COLOR
+        self.pen_width = PEN_SIZE
         self.id = None
 
 
 
 class Line(Shape):
-    # def __init__(self, height: int, width: int, centoid: tuple[int, int]) -> None:
-    #     super().__init__(height,width,centoid)
-    # pass
+    """
+        This is the line class it helps in making and storing the lines made.
+    """
 
     def __init__(self, start_point: tuple[int,int], end_point: tuple[int,int]):
         super().__init__(start_point, end_point)
@@ -35,7 +36,10 @@ class Line(Shape):
             self.orientation = INF
 
     def draw(self, window):
-        self.id = window.canvas.draw_line(self.start_point, self.end_point, color = self.colour, width=PEN_SIZE)
+        """
+            It draws the shape
+        """
+        self.id = window.canvas.draw_line(self.start_point, self.end_point, color = self.colour, width=self.pen_width)
 
     def move(self, new_point: tuple[int,int]):
         '''
@@ -46,6 +50,9 @@ class Line(Shape):
         self.centroid = [(self.start_point[0] + self.end_point[0]) / 2 , (self.start_point[1] + self.end_point[1]) / 2]
 
     def detect_selection(self, point: tuple[int,int]):
+        """
+            It detects whether the click is inside the shape is or not
+        """
         x,y = point
         disl = abs(x-self.start_point[0])+abs(y-self.start_point[1])
         disr = abs(x-self.end_point[0])+abs(y-self.end_point[1])
@@ -59,17 +66,16 @@ class Line(Shape):
 
 
 class Rectangle(Shape):
-    # def __init__(self, height: int, width: int, centoid: tuple[int, int]) -> None:
-    #     super().__init__(height,width,centoid)
-    #     self.type = 0
-    # pass
 
     def __init__(self, start_point: tuple[int,int], end_point: tuple[int,int], corner_type = 's'):
         super().__init__(start_point, end_point)
         self.corner_type = corner_type
 
     def draw(self, window):
-        self.id = window.canvas.draw_rectangle(self.start_point, self.end_point, line_color = self.colour, line_width=PEN_SIZE)
+        """
+            It draws the shape
+        """
+        self.id = window.canvas.draw_rectangle(self.start_point, self.end_point, line_color = self.colour, line_width=self.pen_width)
 
     def move(self, new_point: tuple[int,int]):
         '''
@@ -79,9 +85,10 @@ class Rectangle(Shape):
         self.centroid = [(new_point[0] + self.end_point[0]) / 2 , (new_point[1] + self.end_point[1]) / 2]
 
     def detect_selection(self, point: tuple[int,int]):
+        """
+            It detects whether the click is inside the shape is or not
+        """
         x,y = point
-        # if x <= self.end_point[0] and x >= self.start_point[0] and y <= self.start_point[1] and y >= self.end_point[1]:
-        #     return self
 
         minx = min(self.start_point[0], self.end_point[0])
         miny = min(self.start_point[1], self.end_point[1])
@@ -91,33 +98,3 @@ class Rectangle(Shape):
             return self
         return None
         pass
-
-#
-# class Group(Shape):
-#     def __init__(self, objects: List[Shape]):
-#         self.objects = objects
-#         self.start_point = None
-#         self.end_point = None
-#         self.calculate_bounding_box()
-#
-#     def calculate_bounding_box(self):
-#         min_x = min(obj.start_point[0] for obj in self.objects)
-#         min_y = min(obj.start_point[1] for obj in self.objects)
-#         max_x = max(obj.end_point[0] for obj in self.objects)
-#         max_y = max(obj.end_point[1] for obj in self.objects)
-#         self.start_point = (min_x, min_y)
-#         self.end_point = (max_x, max_y)
-#
-#     def draw(self, window):
-#         for obj in self.objects:
-#             obj.draw(window)
-#
-#     def move(self, dx: int, dy: int):
-#         for obj in self.objects:
-#             obj.move(dx, dy)
-#         self.calculate_bounding_box()
-#     def detect_selection(self, point: tuple[int,int]):
-#         x,y = point
-#         if x <= self.end_point[0] and x >= self.start_point[0] and y <= self.start_point[1] and y >= self.end_point[1]:
-#             return self
-#         pass
