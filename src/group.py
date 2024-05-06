ae07ef15cb142d6050930376a47893f464759401
prakhar479 <singhalprakhar7@gmail.com>
2024-05-06 13:57:36 +0530

    bug fixes

--- src/group.py
+++ src/group.py
@@ -7,25 +7,6 @@ class Group(Object):
     def __init__(self, objects: List[Shape], parent: Optional['Group'] = None):
         self.objects = objects
         self.centroid = [sum([obj.centroid[0] for obj in objects]) / len(objects) , sum([obj.centroid[1] for obj in objects]) / len(objects)]
-        
-        
-        # self.parent = parent
-        # self.children = []  # List of child groups
-        # for obj in objects:
-        #     if isinstance(obj, Group):
-        #         obj.parent = self
-        #         self.children.append(obj)
-        # self.start_point = None
-        # self.end_point = None
-        # self.calculate_bounding_box()
-
-    # def calculate_bounding_box(self):
-    #     min_x = min(obj.start_point[0] for obj in self.objects)
-    #     min_y = min(obj.start_point[1] for obj in self.objects)
-    #     max_x = max(obj.end_point[0] for obj in self.objects)
-    #     max_y = max(obj.end_point[1] for obj in self.objects)
-    #     self.start_point = (min_x, min_y)
-    #     self.end_point = (max_x, max_y)
 
     def draw(self, window):
         for obj in self.objects:
@@ -33,13 +14,6 @@ class Group(Object):
             obj.draw(window)
 
     def move(self, delta: tuple[int, int]):
-        # dx = new_point[0] - self.start_point[0]
-        # dy = new_point[1] - self.start_point[1]
-        # for obj in self.objects:
-        #     obj.move((obj.start_point[0] + dx, obj.start_point[1] + dy))
-        # self.calculate_bounding_box()
-        # for child in self.children:
-        #     child.move(self.start_point)
 
         for obj in self.objects:
             obj.move(delta)
@@ -48,11 +22,6 @@ class Group(Object):
         self.centroid[1] += delta[1]
 
     def detect_selection(self, point: tuple[int, int]):
-        # x, y = point
-        # if x <= self.end_point[0] and x >= self.start_point[0] and y <= self.start_point[1] and y >= self.end_point[1]:
-        #     return self
-        # return None
-
         for obj in self.objects:
             sel = obj.detect_selection(point)
             if sel:
@@ -64,17 +33,17 @@ class Group(Object):
         duplicate_objects = [obj.get_duplicate() for obj in self.objects]
         return Group(duplicate_objects)
     def update_endpoints_randomly(self):
-        a = [10, -10, -5, 5]  # Example values for random update
+        # a = [10, -10, -5, 5]  # Example values for random update
         for obj in self.objects:
-            self._update_endpoints_recursive(obj, a)
+            self._update_endpoints_recursive(obj, -10, 10)
 
-    def _update_endpoints_recursive(self, obj, a):
-        if isinstance(obj, (Line, Rectangle)):
-            kk = random.choice(a)
+    def _update_endpoints_recursive(self, obj, up_bound, low_bound):
+        if isinstance(obj, Shape):
+            kk = random.randint(up_bound, low_bound)
             new_end_point = (obj.end_point[0] + kk, obj.end_point[1] + kk)
             obj.end_point = new_end_point
             new_start_point = (obj.start_point[0] + kk, obj.start_point[1] + kk)
             obj.start_point = new_start_point
         elif isinstance(obj, Group):
             for sub_obj in obj.objects:
-                self._update_endpoints_recursive(sub_obj, a)
+                self._update_endpoints_recursive(sub_obj, up_bound, low_bound)
