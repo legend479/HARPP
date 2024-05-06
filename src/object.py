9aada5bc979a55c1dd428a44f7ebdca084c645cc
Harpreet287 <124236553+Harpreet287@users.noreply.github.com>
2024-05-06 16:48:54 +0530

    pylint: object.py

--- src/object.py
+++ src/object.py
@@ -1,17 +1,24 @@
-import PySimpleGUI as sg
-
+"""
+Objects module defines all things on screen, group or individual, everything
+"""
 class Object():
     """
-        This is the master class for both group and shapes. It provides them a basic structure to start with.
+        This is the master class for both group and shapes.
+         It provides them a basic structure to start with.
     """
     def __init__(self, start_point, end_point):
 
         self.width = end_point[0] - start_point[0]
         self.height = end_point[1] - start_point[1]
-        self.centroid = [(start_point[0] + end_point[0]) / 2 , (start_point[1] + end_point[1]) / 2]
+        self.centroid = [(start_point[0] + end_point[0]) / 2,
+                         (start_point[1] + end_point[1]) / 2]
         self.start_point = start_point
         self.end_point = end_point
 
 
     def contains_point(self, point):
+        """
+        Checks if point is inside
+        the collision bounds of the object
+        """
         raise NotImplementedError("Contains point method not implemented")
